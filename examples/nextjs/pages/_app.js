35acf20f03808ed9efbf032550ec241139485c75
Bruno Antunes <bmvantunes@gmail.com>
2019-10-24 12:06:47 +0100

    Add StrictMode

--- examples/nextjs/pages/_app.js
+++ examples/nextjs/pages/_app.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import App from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from 'react';
 import theme from '../src/theme';
 
 export default class MyApp extends App {
@@ -18,7 +18,7 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <React.Fragment>
+      <React.StrictMode>
         <Head>
           <title>My page</title>
         </Head>
@@ -27,7 +27,7 @@ export default class MyApp extends App {
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
-      </React.Fragment>
+      </React.StrictMode>
     );
   }
 }
